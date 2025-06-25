9bf57432d3daf273c53e9bf3d918dd20e51740e0
Kelvina <Kelvina-web.com>
2025-06-25 03:06:52 +0300

    Adding files

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import pytest
+from config import app, db
 
 def pytest_itemcollected(item):
     par = item.parent.obj
@@ -7,3 +9,12 @@ def pytest_itemcollected(item):
     suf = node.__doc__.strip() if node.__doc__ else node.__name__
     if pref or suf:
         item._nodeid = ' '.join((pref, suf))
+
+@pytest.fixture(autouse=True, scope='session')
+def setup_database():
+    with app.app_context():
+        db.create_all()
+    yield
+    # Optionally, drop tables after tests
+    # with app.app_context():
+    #     db.drop_all()
